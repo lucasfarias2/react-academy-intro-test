6938d08b11d30bdcb921fe91a956e6eee364a139
Lucas Farias <lukf95@gmail.com>
2018-12-23 15:41:42 -0300

    New endpoints

--- app/server/index.js
+++ app/server/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
-server.get('/', (req, res) => res.send('Hello World!'));
+server.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+server.get('/api/items', (req, res) => {
+  const query = req.query.q;
+  console.log(req.query);
+  res.send(`JSON de listado. Search term: ${query}`);
+});
+
+server.get('/api/items/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`JSON de item. ItemId: ${id}`);
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}!`));
