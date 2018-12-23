028cab904b8eceac5f83243a005c6d0fbcf25b9e
Lucas Farias <lukf95@gmail.com>
2018-12-23 17:09:04 -0300

    Search logic

--- app/server/index.js
+++ app/server/index.js
@@ -7,15 +7,47 @@ server.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const getDecimalsFromPrice = amount =>
+  amount % 1 !== 0 ? parseInt(amount.toString().split('.')[1]) : 0;
+
 server.get('/api/items', (req, res) => {
   const query = req.query.q;
-  console.log(req.query);
-  res.send(`JSON de listado. Search term: ${query}`);
+  axios
+    .get(`https://api.mercadolibre.com/sites/MLA/search`, {
+      params: { q: query, limit: 4 }
+    })
+    .then(response => {
+      const items = [];
+      response.data.results.forEach((item) => {
+        const currentItem = {
+          id: item.id,
+          title: item.title,
+          price: {
+            currency: item.currency_id,
+            amount: Math.trunc(item.price),
+            decimals: getDecimalsFromPrice(item.price)
+          },
+          picture: item.thumbnail,
+          condition: item.condition,
+          free_shipping: item.shipping.free_shipping,
+        }
+        items.push(currentItem);
+      })
+      const listingDto = {
+        author: { name: 'Lucas', lastname: 'Farías' },
+        items,
+      };
+      res.json(listingDto);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 server.get('/api/items/:id', (req, res) => {
+  const id = req.params.id;
   axios
-    .get(`http://api.mercadolibre.com/items/${req.params.id}`)
+    .get(`http://api.mercadolibre.com/items/${id}`)
     .then(response => {
       const itemDto = {
         author: { name: 'Lucas', lastname: 'Farías' },
@@ -24,14 +56,14 @@ server.get('/api/items/:id', (req, res) => {
           title: response.data.title,
           price: {
             currency: response.data.currency_id,
-            amount: response.data.price,
-            decimals: 0
+            amount: Math.trunc(response.data.price),
+            decimals: getDecimalsFromPrice(response.data.price)
           },
-          picture: response.data.permalink,
+          picture: response.data.thumbnail,
           condition: response.data.condition,
           free_shipping: response.data.shipping.free_shipping,
           sold_quantity: response.data.sold_quantity,
-          descripcion: 'Descripcion'
+          descripcion: 'Descripción'
         }
       };
       res.json(itemDto);
