359065319f2869722a740de721fdfec049da7071
Lucas Farias <lukf95@gmail.com>
2018-12-23 16:12:51 -0300

    Item map response

--- app/server/index.js
+++ app/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const server = express();
 const port = 3000;
 
@@ -13,8 +14,31 @@ server.get('/api/items', (req, res) => {
 });
 
 server.get('/api/items/:id', (req, res) => {
-  const id = req.params.id;
-  res.send(`JSON de item. ItemId: ${id}`);
+  axios
+    .get(`http://api.mercadolibre.com/items/${req.params.id}`)
+    .then(response => {
+      const itemDto = {
+        author: { name: 'Lucas', lastname: 'Farías' },
+        item: {
+          id: response.data.id,
+          title: response.data.title,
+          price: {
+            currency: response.data.currency_id,
+            amount: response.data.price,
+            decimals: 0
+          },
+          picture: response.data.permalink,
+          condition: response.data.condition,
+          free_shipping: response.data.shipping.free_shipping,
+          sold_quantity: response.data.sold_quantity,
+          descripcion: 'Descripcion'
+        }
+      };
+      res.json(itemDto);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}!`));
