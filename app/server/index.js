d5574cebb8a4fb83bb4205e3df1844ce0a602b6c
Lucas Farias <lukf95@gmail.com>
2018-12-23 17:45:43 -0300

    Refactor

--- app/server/index.js
+++ app/server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const { getItem, getItemListing } = require('./services/item-service');
 const server = express();
 const port = 3000;
 
@@ -7,70 +7,16 @@ server.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const getDecimalsFromPrice = amount =>
-  amount % 1 !== 0 ? parseInt(amount.toString().split('.')[1]) : 0;
-
 server.get('/api/items', (req, res) => {
-  const query = req.query.q;
-  axios
-    .get(`https://api.mercadolibre.com/sites/MLA/search`, {
-      params: { q: query, limit: 4 }
-    })
-    .then(response => {
-      const items = [];
-      response.data.results.forEach((item) => {
-        const currentItem = {
-          id: item.id,
-          title: item.title,
-          price: {
-            currency: item.currency_id,
-            amount: Math.trunc(item.price),
-            decimals: getDecimalsFromPrice(item.price)
-          },
-          picture: item.thumbnail,
-          condition: item.condition,
-          free_shipping: item.shipping.free_shipping,
-        }
-        items.push(currentItem);
-      })
-      const listingDto = {
-        author: { name: 'Lucas', lastname: 'Farías' },
-        items,
-      };
-      res.json(listingDto);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  getItemListing(req.query.q)
+    .then(response => res.json(response))
+    .catch(err => res.sendStatus(500, err));
 });
 
 server.get('/api/items/:id', (req, res) => {
-  const id = req.params.id;
-  axios
-    .get(`http://api.mercadolibre.com/items/${id}`)
-    .then(response => {
-      const itemDto = {
-        author: { name: 'Lucas', lastname: 'Farías' },
-        item: {
-          id: response.data.id,
-          title: response.data.title,
-          price: {
-            currency: response.data.currency_id,
-            amount: Math.trunc(response.data.price),
-            decimals: getDecimalsFromPrice(response.data.price)
-          },
-          picture: response.data.thumbnail,
-          condition: response.data.condition,
-          free_shipping: response.data.shipping.free_shipping,
-          sold_quantity: response.data.sold_quantity,
-          descripcion: 'Descripción'
-        }
-      };
-      res.json(itemDto);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  getItem(req.params.id)
+    .then(response => res.json(response))
+    .catch(err => res.sendStatus(500, err));
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}!`));
---
+++ app/server/services/item-service.js
@@ -0,0 +1,67 @@
+const axios = require('axios');
+
+const getDecimalsFromPrice = amount =>
+  amount % 1 !== 0 ? parseInt(amount.toString().split('.')[1]) : 0;
+
+const getItemListing = async query => {
+  try {
+    const response = await axios.get(
+      `https://api.mercadolibre.com/sites/MLA/search`,
+      {
+        params: { q: query, limit: 4 }
+      }
+    );
+    const items = [];
+    response.data.results.forEach(item => {
+      const currentItem = {
+        id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: Math.trunc(item.price),
+          decimals: getDecimalsFromPrice(item.price)
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping
+      };
+      items.push(currentItem);
+    });
+    const listingDto = {
+      author: { name: 'Lucas', lastname: 'Farías' },
+      categories: response.data.results.map(item => item.category_id),
+      items
+    };
+    return listingDto;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const getItem = async id => {
+  try {
+    const response = await axios.get(`http://api.mercadolibre.com/items/${id}`);
+    const itemDto = {
+      author: { name: 'Lucas', lastname: 'Farías' },
+      item: {
+        id: response.data.id,
+        title: response.data.title,
+        price: {
+          currency: response.data.currency_id,
+          amount: Math.trunc(response.data.price),
+          decimals: getDecimalsFromPrice(response.data.price)
+        },
+        picture: response.data.thumbnail,
+        condition: response.data.condition,
+        free_shipping: response.data.shipping.free_shipping,
+        sold_quantity: response.data.sold_quantity,
+        descripcion: 'Descripción'
+      }
+    };
+    return itemDto;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = { getItem, getItemListing };
